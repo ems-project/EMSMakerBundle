46a62fb6f57ad1db773a2ad20113d23080782bd4
Mathieu De Keyzer <me@theus.be>
2021-01-18 17:01:50 +0100

    feat: analyzers maker

--- Command/AnalyzerCommand.php
+++ Command/AnalyzerCommand.php
@@ -2,16 +2,58 @@
 
 namespace EMS\MakerBundle\Command;
 
-use Symfony\Component\Console\Command\Command;
+use EMS\CoreBundle\Entity\Helper\JsonClass;
+use EMS\CoreBundle\Repository\AnalyzerRepository;
+use EMS\MakerBundle\Service\FileService;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class AnalyzerCommand extends Command
+class AnalyzerCommand extends AbstractCommand
 {
-    protected static $defaultName = 'ems:make:analyzer';
+    protected static $defaultName = 'ems:maker:analyzer';
+
+    private AnalyzerRepository $analyzerRepository;
+    private FileService $fileService;
+
+    public function __construct(AnalyzerRepository $analyzerRepository, FileService $fileService)
+    {
+        $this->analyzerRepository = $analyzerRepository;
+        $this->fileService = $fileService;
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->makeAnalyzers($this->config[AbstractCommand::ANALYZERS]);
+
+        return 0;
+    }
 
     /**
-     * create an analyzer based on a selection (input) in the Resource/make/analyzer folder.
-     * --help should include the list of files in that folder
-     *
-     * Pass the desired languages to the command (default EN only) (--all ?)
+     * @param string[] $analyzers
      */
+    private function makeAnalyzers(array $analyzers): void
+    {
+        if (\count($analyzers) === 0) {
+            return;
+        }
+        $this->io->title('Make analyzers');
+        $this->io->progressStart(\count($analyzers));
+        foreach ($analyzers as $filename) {
+            $json = $this->fileService->getFileContentsByFileName($filename, FileService::TYPE_ANALYZER);
+            $analyzer = $this->analyzerRepository->findByName($filename);
+
+            if (null !== $analyzer && !$this->forceUpdate('analyzer', $filename)) {
+                $this->io->note(\sprintf('Analyzer %s ignored', $filename));
+                $this->io->progressAdvance();
+                continue;
+            }
+
+            $meta = JsonClass::fromJsonString($json);
+            $analyzer = $meta->jsonDeserialize($analyzer);
+            $this->analyzerRepository->update($analyzer);
+            $this->io->progressAdvance();
+        }
+    }
+
 }
